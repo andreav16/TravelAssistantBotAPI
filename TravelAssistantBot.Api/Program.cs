3b2ef6dbb8577e44f8340e9a40f6301d35cb873f
andreasmm16 <andreasmm@unitec.edu>
2023-06-11 08:50:11 -0600

    cors info added

--- TravelAssistantBot.Api/Program.cs
+++ TravelAssistantBot.Api/Program.cs
@@ -2,6 +2,7 @@ using TravelAssistantBot.Core;
 using TravelAssistantBot.Core.EventManager;
 
 var builder = WebApplication.CreateBuilder(args);
+var allowAllOriginsPolicy = "_allowAllOriginsPolicy";
 
 // Add services to the container
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,7 +10,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEventService, EventService>();
 builder.Services.AddAutoMapper(typeof(Program));
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowAllOriginsPolicy,
+                      policy =>
+                      {
+                          policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                      });
+});
 
 builder.Services.AddControllers();
 
@@ -21,6 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(allowAllOriginsPolicy);
 
 app.UseHttpsRedirection();
 
