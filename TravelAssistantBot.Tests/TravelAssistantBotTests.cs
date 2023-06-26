04833ff187c9a2107cd301e7144555c7bef4d3b4
andreasmm16 <andreasmm@unitec.edu>
2023-06-25 23:58:39 -0600

    dos pruebas hechas

--- TravelAssistantBot.Tests/TravelAssistantBotTests.cs
+++ TravelAssistantBot.Tests/TravelAssistantBotTests.cs
@@ -1,76 +1,112 @@
-using NUnit.Framework;
+using FluentAssertions;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using TravelAssistantBot.Core.Entities.GeopifyEntities.CountryEntities;
+using System.Reflection;
 using TravelAssistantBot.Core;
-using TravelAssistantBot.Core.GeopifyManager;
-using Assert = NUnit.Framework.Assert;
+using TravelAssistantBot.Core.Entities;
+using TravelAssistantBot.Core.Entities.FlightEntities;
+using TravelAssistantBot.Core.EventManager;
+using TravelAssistantBot.Infrastructure.EntityFramework;
+using TravelAssistantBot.Infrastructure.EntityFramework.Repositories;
+using TypeMock.ArrangeActAssert;
+using Xunit;
+
 
 namespace TravelAssistantBot.Tests;
 public class GeocodeServiceTests
 {
-    private GeopifyService geocodeService;
-    private Mock<HttpClient> httpClientMock;
-    private GeopifyOptions_Geocode geopifyOptions;
-
-    [SetUp]
-    public void Setup()
+ 
+    [Theory]
+    [InlineData("American Airlines", 1)]
+    public void FilterAirportByName_ReturnsCorrectResults(string name, int expectedCount)
     {
-        httpClientMock = new Mock<HttpClient>();
-        geopifyOptions = new GeopifyOptions_Geocode
+        var airlines = new List<Airline>
         {
-            BaseUrl = geopifyOptions.BaseUrl,
-            ApiKey = geopifyOptions.ApiKey
+            new Airline
+            {
+                Name = "American Airlines",
+                Id = 1,
+                IATA = "AA",
+                FlightId = 1,
+            },
+            new Airline
+            {
+                Name = "Copa Airlines",
+                Id = 2,
+                IATA = "CM",
+                FlightId = 2,
+            },
+            new Airline
+            {
+                Name = "Delta Air Lines",
+                Id = 3,
+                IATA = "DL",
+                FlightId = 3,
+            },
+            new Airline
+            {
+                Name = "United Airlines",
+                Id = 4,
+                IATA = "UA",
+                FlightId = 4,
+            },
+            new Airline
+            {
+                Name = "Air France",
+                Id = 5,
+                IATA = "AF",
+                FlightId = 5,
+            },
         };
-        geocodeService = new GeopifyService(httpClientMock.Object, geopifyOptions);
+
+        TravelAssistantBotContext context = GetTravelAssistantBotContext(airlines, "FilterAirportByName");
+
+        var repository = new BaseRepository<Airline>(context);
+
+        var result = repository.Filter(x => x.Name.StartsWith(name));
+
+        result.Count().Should().Be(expectedCount);
     }
 
-    [Test]
-    public async Task GetCountryAsync_WhenSuccessfulResponse_ReturnsGeocodeGroup()
+    [Fact]
+    public async Task GetEventByName_ValidName_ReturnCorrectEvent()
     {
         // Arrange
-        string cityName = "London";
-        string apiUrl = $"{geopifyOptions.BaseUrl}?apiKey={geopifyOptions.ApiKey}&text={cityName}";
-
-        GeocodeGroup expectedGeocodes = new GeocodeGroup
+        var eventServiceMock = new Mock<IEventService>();
+        eventServiceMock.Setup(e => e.GetEventByNameAsync("Go to the doctor")).ReturnsAsync(new Event
         {
-            // Set your expected geocode data here
-        };
+            Id = "1",
+            Summary = "Go to the doctor",
+            Start = new EventDateTime { DateTime = DateTime.Now.AddDays(1) },
+            End = new EventDateTime { DateTime = DateTime.Now.AddDays(2) }
+        });
 
-        HttpResponseMessage successfulResponse = new HttpResponseMessage(HttpStatusCode.OK);
-        successfulResponse.Content = new StringContent(JsonConvert.SerializeObject(expectedGeocodes));
-
-        httpClientMock
-            .Setup(client => client.GetAsync(apiUrl, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(successfulResponse);
+        var eventService = eventServiceMock.Object;
 
         // Act
-        OperationResult<GeocodeGroup> result = await geocodeService.GetCountryAsync(cityName);
+        var result = await eventService.GetEventByNameAsync("Go to the doctor");
 
         // Assert
-        Assert.IsNotNull(result);
-        Assert.IsTrue(result.Succeeded);
-        Assert.AreEqual(expectedGeocodes, result.Result);
+        Xunit.Assert.NotNull(result);
+        Xunit.Assert.Equal("Go to the doctor", result.Result.Summary);
     }
 
-    [Test]
-    public void GetCountryAsync_WhenUnsuccessfulResponse_ThrowsException()
+    private TravelAssistantBotContext GetTravelAssistantBotContext(List<Airline> airlines, string name)
     {
-        // Arrange
-        string cityName = "London";
-        string apiUrl = $"{geopifyOptions.BaseUrl}?apiKey={geopifyOptions.ApiKey}&text={cityName}";
 
-        HttpResponseMessage unsuccessfulResponse = new HttpResponseMessage(HttpStatusCode.BadRequest);
+      
+        var contextOptions = new DbContextOptionsBuilder<TravelAssistantBotContext>()
+            .UseInMemoryDatabase(databaseName: name)
+            .Options;
 
-        httpClientMock
-            .Setup(client => client.GetAsync(apiUrl, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(unsuccessfulResponse);
-
-        // Act & Assert
-        Assert.ThrowsAsync<Exception>(() => geocodeService.GetCountryAsync(cityName));
+        var context = new TravelAssistantBotContext(contextOptions);
+        context.Airlines.AddRange(airlines);
+        context.SaveChanges();
+        return context;
     }
 }
